c7ee1b6c4a8475154b59c31de23285b664803c6a
Vivek Raj <vivekraj@VivekRaj-PC.local>
2024-08-30 00:48:34 +0530

    Added name Vivek to PHP code

--- index.php
+++ index.php
@@ -4,7 +4,13 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+// Code maintained by Vivek
+
 require 'includes/config.php';
 require 'includes/functions.php';
 
+// Print name for debugging or display purposes
+echo "Developed by Vivek";
+
 init();
+
